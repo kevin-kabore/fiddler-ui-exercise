5f5e7ce383093027098266bd4d4d0bebdcf95a6f
Kevin Kabore <kevin.s.kabore@gmail.com>
2021-05-26 01:51:20 -0700

    chore: organize, add comments

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,37 +1,28 @@
 import * as React from 'react'
 
 const TabsContext = React.createContext()
-
-function TabPanel({children}) {
-  return <div>{children}</div>
-}
-
-function TabPanels({children, ...props}) {
-  const [activeIndex] = React.useContext(TabsContext)
-  return (
-    <div>
-      {children.map((panel, idx) => (idx === activeIndex ? panel : null))}
-    </div>
-  )
-}
-
-function Tab({children: child, onClick}) {
-  return <button onClick={onClick}>{child}</button>
-}
-
-function TabList({children, ...props}) {
-  const [, setState] = React.useContext(TabsContext)
-  return React.Children.map(children, (child, index) => {
-    return React.cloneElement(child, {
-      key: `tab-${index}`,
-      onClick: () => {
-        setState(index)
-        child.props?.onClick && child.props.onClick()
-      },
-    })
-  })
-}
-
+/**
+ * A declarative and bare bones Tabs component usage:
+ * <Tabs>
+ *  <TabList>
+ *    <Tab>1</Tab>
+ *    <Tab>2</Tab>
+ *    <Tab>3</Tab>
+ *  </TabList>
+ *
+ *  <TabPanels>
+ *    <TabPanel>
+ *      <p>one!</p>
+ *    </TabPanel>
+ *    <TabPanel>
+ *      <p>two!</p>
+ *    </TabPanel>
+ *    <TabPanel>
+ *      <p>three!</p>
+ *    </TabPanel>
+ *  </TabPanels>
+ * </Tabs>
+ */
 function Tabs({children, ...props}) {
   const [state, setState] = React.useState(0)
   return (
@@ -39,6 +30,42 @@ function Tabs({children, ...props}) {
       {children}
     </TabsContext.Provider>
   )
+}
+
+function TabList({children, ...props}) {
+  const [, setState] = React.useContext(TabsContext)
+  return React.Children.map(children, (child, index) => {
+    // clone each child with an additional onClick to set the active index
+    return React.cloneElement(child, {
+      key: `tab-${index}`,
+      onClick: () => {
+        setState(index)
+        // so we don't forget to call any onClick passed to the <Tab />!
+        child.props?.onClick && child.props.onClick()
+      },
+    })
+  })
+}
+
+function Tab({children: child, onClick, ...props}) {
+  return (
+    <button {...props} onClick={onClick}>
+      {child}
+    </button>
+  )
+}
+
+function TabPanels({children, ...props}) {
+  const [activeIndex] = React.useContext(TabsContext)
+  return (
+    <div {...props}>
+      {children.map((panel, idx) => (idx === activeIndex ? panel : null))}
+    </div>
+  )
+}
+
+function TabPanel({children, ...props}) {
+  return <div {...props}>{children}</div>
 }
 
 export {Tabs, TabList, Tab, TabPanel, TabPanels}
